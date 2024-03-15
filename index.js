14a0b71d1e1fcbe8491d27ea3d2d3eaf96a0233d
RoKerjea <kerjeanr@gmail.com>
2024-03-15 14:13:49 +0100

    feat: first random world working

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hello world");
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getSalutation() {
+    switch (getRandomInt(3)) {
+        case 0:
+            return("Hello");
+        case 1:
+            return("Bonjour");
+        case 2:
+            return("Demat");
+    }
+}
+// console.log("hello world");
+
+async function main() {
+	
+    const string1 = getSalutation();
+    const string2 = "World";
+    const string3 = "!";
+    const res = string1  + " " +string2 + " " + string3;
+    console.log(res);   
+}
+
+main()
+    .catch(err => {
+        console.error("Error.");
+        console.error(err && err.stack || err);
+    });
